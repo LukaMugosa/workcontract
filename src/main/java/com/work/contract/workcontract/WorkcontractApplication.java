cf0aa1a4b2610218bbf2d58f2d6f1dd6605fc1eb
LukaMugosa <lukamugosa1945@gmail.com>
2023-06-02 19:46:41 +0200

    first commit

---
+++ src/main/java/com/work/contract/workcontract/WorkcontractApplication.java
@@ -0,0 +1,13 @@
+package com.work.contract.workcontract;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkcontractApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkcontractApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/work/contract/workcontract/WorkcontractApplicationTests.java
@@ -0,0 +1,13 @@
+package com.work.contract.workcontract;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkcontractApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
